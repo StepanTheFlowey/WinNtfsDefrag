6c524b5a957d820d87af63c980658666a051fda1
StepanTheFlowey <stepanmasirov@gmail.com>
2023-03-21 15:15:02 +0500

    bitmap

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include <Windows.h>
 #include <vector>
 
+uint32_t getVolumeClusterCount(const std::string& volumePath) {
+  DWORD sectorsPerCluster;
+  DWORD bytesPerSector;
+  DWORD numbersOfFreeClusters;
+  DWORD totalNumberOfClusters;
+
+  GetDiskFreeSpaceA(
+    volumePath.c_str(),
+    &sectorsPerCluster,
+    &bytesPerSector,
+    &numbersOfFreeClusters,
+    &totalNumberOfClusters
+  );
+
+  return totalNumberOfClusters;
+}
+
 uint64_t getVolumeSize(const HANDLE volumeHandle) {
   DISK_GEOMETRY_EX geometry;
   DWORD bytesReturned;
@@ -24,38 +41,51 @@ uint64_t getVolumeSize(const HANDLE volumeHandle) {
   return geometry.DiskSize.QuadPart;
 }
 
-void getVolumeBitmap(const HANDLE volumeHandle, std::vector<uint8_t>& bitmap) {
-  uint64_t bytesWanted = getVolumeSize(volumeHandle);
-  DWORD bytesReturned;
+VOLUME_BITMAP_BUFFER* getVolumeBitmap(const HANDLE volumeHandle, uint32_t& bitmapSize) {
+  STARTING_LCN_INPUT_BUFFER sStartLcn{
+    .StartingLcn = 0
+  };
 
-  bitmap.resize(bytesWanted);
+  DWORD dwBitmapSize = 0;
+  DWORD dwAllocatedSize = 64 * 1024;
+  VOLUME_BITMAP_BUFFER* pVolumeBitmap = NULL;
+  while(true) {
+    pVolumeBitmap = reinterpret_cast<VOLUME_BITMAP_BUFFER*>(new uint8_t[dwAllocatedSize]);
 
-  BOOL result = DeviceIoControl(
-    volumeHandle,
-    FSCTL_GET_VOLUME_BITMAP,
-    NULL,
-    0,
-    bitmap.data(),
-    bitmap.size(),
-    &bytesReturned,
-    NULL
-  );
+    BOOL result = DeviceIoControl(
+      volumeHandle,
+      FSCTL_GET_VOLUME_BITMAP,
+      &sStartLcn,
+      sizeof(sStartLcn),
+      pVolumeBitmap,
+      dwAllocatedSize,
+      &dwBitmapSize,
+      NULL
+    );
 
-  if(!result) {
-    DebugBreak();
+    if(result) {
+      bitmapSize = dwBitmapSize;
+      return pVolumeBitmap;
+    }
+
+    if(GetLastError() != ERROR_MORE_DATA) {
+      delete[] pVolumeBitmap;
+      return nullptr;
+    }
+
+    delete[] pVolumeBitmap;
+
+    dwAllocatedSize *= 2;
   }
 }
 
-int main(int argc, char** argv) {
-  char volumePath[128];
-  GetVolumeNameForVolumeMountPointA(
-    "G:\\",
-    volumePath,
-    sizeof(volumePath)
-  );
+bool getBitmapValue(PVOLUME_BITMAP_BUFFER bitmap, const uintptr_t index) {
+  return (bitmap->Buffer[index / 8] >> index % 8) & 1;
+}
 
+int main(int argc, char** argv) {
   HANDLE volumeHandle = CreateFileA(
-    volumePath,
+    "\\\\.\\G:",
     GENERIC_READ | GENERIC_WRITE,
     FILE_SHARE_READ | FILE_SHARE_WRITE,
     NULL,
@@ -65,14 +95,26 @@ int main(int argc, char** argv) {
   );
 
   if(volumeHandle == INVALID_HANDLE_VALUE) {
-    std::cout << "Error: " << GetLastError() << std::endl;
+    std::cout << "Failed to open volume.\nError code: " << GetLastError() << std::endl;
     DebugBreak();
     return EXIT_FAILURE;
   }
 
-  std::vector<uint8_t> volumeBitmap;
+  uint32_t bitmapSize = 0;
+  VOLUME_BITMAP_BUFFER* volumeBitmap = getVolumeBitmap(volumeHandle, bitmapSize);
 
-  getVolumeBitmap(volumeHandle, volumeBitmap);
+  for(uintptr_t i = 0; i < 500; ++i) {
+    for(uintptr_t j = 0; j < 100; ++j) {
+      float index = bitmapSize / (500.F * 100.F) * (j + i * 100.F);
+
+      std::cout.put(getBitmapValue(volumeBitmap, static_cast<uintptr_t>(index)) ? '#' : '-');
+    }
+    std::cout.put('\n');
+  }
+
+  std::cout.flush();
+
+  delete[] volumeBitmap;
 
   return EXIT_SUCCESS;
 }
